aac621dbdae1a80da0fdee3a144ce96c39a46976
Abhishek <aabhishek22>
2020-06-29 07:17:47 +0530

    Updates

---
+++ precache-manifest.d7d8ffa905f9146c817ab56ce9c69cea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b01464506a7c80ce04f3788324a0761",
+    "url": "/To-do-react/index.html"
+  },
+  {
+    "revision": "c2a455815bd56a5bd73c",
+    "url": "/To-do-react/static/css/main.dd68f31a.chunk.css"
+  },
+  {
+    "revision": "75b213236662a7a249ec",
+    "url": "/To-do-react/static/js/2.21a4089c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-react/static/js/2.21a4089c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2a455815bd56a5bd73c",
+    "url": "/To-do-react/static/js/main.50c78288.chunk.js"
+  },
+  {
+    "revision": "a859f48f8499d6f608a5",
+    "url": "/To-do-react/static/js/runtime-main.f42973ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-do-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
